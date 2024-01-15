0e7ec6983e7854fd1a37e86b1b2ca62a31d52640
Kevin Zou <kevin.zp.zou@gmail.com>
2024-01-15 00:25:58 -0800

    09: Add unique display names and active user count

--- 09_chat/index.js
+++ 09_chat/index.js
@@ -10,14 +10,37 @@ const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const usernames = new Map();
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
+  socket.join(socket.id); // For sending messages to this user only
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const username = usernames.get(socket.id);
+    io.emit('chat message', { msg, username });
   });
+
+  socket.on('new username', (username) => {
+    const takenUsernames = Array.from(usernames.values());
+    if (takenUsernames.includes(username)) {
+      io.to(socket.id).emit('username taken');
+    } else {
+      usernames.set(socket.id, username);
+      io.to(socket.id).emit('username ok');
+      io.emit('updated users', Array.from(usernames.values()));
+    }
+  });
+
+  socket.on('disconnect', () => {
+    usernames.delete(socket.id);
+    io.emit('updated users', Array.from(usernames.values()));
+  })
 });
 
 server.listen(3000, () => {
